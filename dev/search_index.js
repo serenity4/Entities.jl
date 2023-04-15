11b7803b886bd035c2680b383d6192cc884f574d
Documenter.jl <documenter@juliadocs.github.io>
2023-04-15 11:20:59 +0000

    build based on b8191fc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Entities","category":"page"},{"location":"#Entities","page":"Home","title":"Entities","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Entities.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Entities]","category":"page"}]
+}
